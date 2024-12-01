1fe82880dd846555f6347a4edee9fd6a2615b4f4
SS56Bo <sohamsarkar560@gmail.com>
2024-12-02 01:42:44 +0530

    Update

--- Cards/deck.go
+++ Cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 //create a new type of 'deck'
 type deck []string
@@ -28,4 +32,12 @@ func (d deck) print(){
 
 func deal(d deck, handSize int ) (deck, deck){
 	 return d[:handSize], d[handSize:]
+}
+
+func  (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func  (d deck) toHardDrive(fileName string) error{
+	return os.WriteFile(fileName, []byte(d.toString()), 0666)
 }
--- Cards/main.go
+++ Cards/main.go
@@ -1,12 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
 
-	hands, remainingCards := deal(cards, 5)
-	hands.print()
-	fmt.Println(" ")
-	remainingCards.print()
+	// result := cards.toString()
+	cards.toHardDrive("my_cards")
 }
